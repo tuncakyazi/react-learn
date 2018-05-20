57047f468a40f6d8895903b8b92bb8be9aaec016
tuncakyazi <akyazi.tunc@gmail.com>
2018-05-21 01:16:54 +0300

    readme

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import App from './../App';
+
+configure({ adapter: new Adapter() });
+
+it('renders correctly', () => {
+  const snap = shallow(<App />);
+
+  expect(snap).toMatchSnapshot();
+});
